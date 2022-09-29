90b90695eabdb6d159f992eb01156da91d83a271
Aman Kumar <kuma0181@algonquinlive.com>
2022-09-28 20:24:08 -0400

    Finished week 2

--- app/src/main/java/algonquin/cst2335/kuma0181/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/kuma0181/MainActivity.java
@@ -3,12 +3,55 @@ package algonquin.cst2335.kuma0181;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import algonquin.cst2335.kuma0181.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        TextView textView = binding.textview;
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                textView.setText("Welcome to Cst3463");
+                String editString = binding.myedittext.getText().toString();
+                textView.setText("Your edit text has:  " + editString);
+
+            }
+        });
+
+        binding.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Image view height : "+binding.imageView.getHeight() + ". Image view width : "+binding.imageView.getWidth(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(MainActivity.this, "Radio button status is: "+b, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        binding.switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b==true){
+                    Toast.makeText(MainActivity.this, "Switch button status is selected.", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Switch button status is not selected.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
